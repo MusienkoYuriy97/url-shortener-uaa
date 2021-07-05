f84b6f05b5854cb40211db635504a9de274470e4
MusienkoYuriy97 <97musienko@gmail.com>
2021-07-05 15:14:26 +0300

    add data-postgres.sql, change application.properties, change schema-postgres.sql

---
+++ src/main/resources/data-postgres.sql
@@ -0,0 +1,17 @@
+INSERT INTO users (email,first_name,last_name,password,user_role,user_status)
+values (
+       '97musienko@gmail.com',
+        'Yuriy',
+        'Musienko',
+        '$2y$12$QKQ2d03.6SCPEQhLlaZusOJT9Q/ODqnvFHT6JCdpqvxKeXHa7KviG',
+        'ADMIN',
+        'ACTIVE'
+        ),
+       (
+           'pavel@gmail.com',
+           'Pavel',
+           'Pavlov',
+           '$2y$12$QKQ2d03.6SCPEQhLlaZusOJT9Q/ODqnvFHT6JCdpqvxKeXHa7KviG',
+           'USER',
+           'ACTIVE'
+       );
