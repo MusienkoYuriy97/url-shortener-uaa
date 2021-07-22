838bf380311c97ed4c0d30c65d8c7169b9508a48
MusienkoYuriy97 <97musienko@gmail.com>
2021-07-22 19:13:06 +0300

    resource and utils for test

---
+++ src/test/java/by/solbegsoft/urlshorteneruaa/util/UserConstant.java
@@ -0,0 +1,21 @@
+package by.solbegsoft.urlshorteneruaa.util;
+
+import java.util.UUID;
+
+public class UserConstant {
+    public static final UUID USER_UUID = UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
+    public static final String FIRST_USER_NAME = "Yury";
+    public static final String LAST_USER_NAME = "Musienko";
+    public static final String USER_EMAIL = "musienko@gmail.com";
+    public static final String USER_PASSWORD = "1111";
+    public static final String USER_NEW_PASSWORD = "1234";
+    public static final String SIMPLE_ACTIVATE_KEY = "lB1wI3gasvMH";
+
+    public static final UUID ADMIN_UUID = UUID.fromString("dfb5305a-24ca-42f2-84e7-3edc5edd8e29");
+    public static final String FIRST_ADMIN_NAME = "Alexey";
+    public static final String LAST_ADMIN_NAME = "Ivanov";
+    public static final String ADMIN_EMAIL = "admin@gmail.com";
+    public static final String ADMIN_PASSWORD = "admin";
+    public static String BEARER_ADMIN_TOKEN = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5N211c2llbmtvQGdtYWlsLmNvbSIsInVzZXJSb2xlIjoiUk9MRV9BRE1JTiIsImlkIjoiNDZmYzdjYjgtZGUxNS00ZmU4LTk2ZTQtODVmNjgyYzM4MmJmIiwiaWF0IjoxNjI2OTM5MjgyLCJleHAiOjE2Mjc1NDQwODJ9.QQ0XphY0qyVj0fSS7N8H-XhkX4XULpHoKLM8cXnxNYQ";
+    public static String BEARER_USER_TOKEN = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwYXZlbEBnbWFpbC5jb20iLCJ1c2VyUm9sZSI6IlJPTEVfVVNFUiIsImlkIjoiYzMxZWY5YjctNDc0My00YWY0LTljYTYtNDZmOTkwY2JlMWY5IiwiaWF0IjoxNjI2OTYxOTk3LCJleHAiOjE2Mjc1NjY3OTd9.rLQZNABpVF0r7ArOdNRYSP2NZkj0rePKTwYQmDGZ0RU";
+}
