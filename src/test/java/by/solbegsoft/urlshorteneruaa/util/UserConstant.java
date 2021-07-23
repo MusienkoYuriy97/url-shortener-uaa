32fc00a89c6bc4d37e80a332cad2aaf0549c077a
MusienkoYuriy97 <97musienko@gmail.com>
2021-07-23 09:48:50 +0300

    auto generate uuid and simple activate key

--- src/test/java/by/solbegsoft/urlshorteneruaa/util/UserConstant.java
+++ src/test/java/by/solbegsoft/urlshorteneruaa/util/UserConstant.java
@@ -2,16 +2,18 @@ package by.solbegsoft.urlshorteneruaa.util;
 
 import java.util.UUID;
 
+import static by.solbegsoft.urlshorteneruaa.common.StringGenerator.*;
+
 public class UserConstant {
-    public static final UUID USER_UUID = UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
+    public static final UUID USER_UUID = UUID.randomUUID();
     public static final String FIRST_USER_NAME = "Ivan";
     public static final String LAST_USER_NAME = "Pavlov";
     public static final String USER_EMAIL = "musienko@gmail.com";
     public static final String USER_PASSWORD = "1111";
     public static final String USER_NEW_PASSWORD = "1234";
-    public static final String SIMPLE_ACTIVATE_KEY = "lB1wI3gasvMH";
+    public static final String SIMPLE_ACTIVATE_KEY = generate(10);
 
-    public static final UUID ADMIN_UUID = UUID.fromString("dfb5305a-24ca-42f2-84e7-3edc5edd8e29");
+    public static final UUID ADMIN_UUID = UUID.randomUUID();
     public static final String FIRST_ADMIN_NAME = "Yuriy";
     public static final String LAST_ADMIN_NAME = "Musienko";
     public static final String ADMIN_EMAIL = "97musienko@gmail.com";
