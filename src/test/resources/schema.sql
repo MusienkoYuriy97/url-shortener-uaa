c607d7da9679e044e02827d9e84686d17ac549a2
MusienkoYuriy97 <97musienko@gmail.com>
2021-08-04 15:37:14 +0300

    change schema.sql

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -15,6 +15,7 @@ CREATE TABLE IF NOT EXISTS activate_key(
     uuid UUID DEFAULT gen_random_uuid(),
     simple_key VARCHAR (15) NOT NULL UNIQUE,
     user_uuid UUID NOT NULL,
+    created_at TIMESTAMP NOT NULL,
     PRIMARY KEY (uuid),
     FOREIGN KEY (user_uuid)
     REFERENCES users (uuid));
